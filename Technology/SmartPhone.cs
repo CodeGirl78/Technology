faceae338ff372298b21f9d99a68e506a60d575f
Melinda <codegirl78@gmail.com>
2021-08-17 20:51:45 -0500

    Completed exercises with unit tests

--- Technology/SmartPhone.cs
+++ Technology/SmartPhone.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Technology
+﻿namespace Technology
 {
-    class SmartPhone
+    public class SmartPhone : Computer
     {
+        public string Carrier { get; set; }
+        private bool serviceActivated;
+        public readonly string SerialNumber = "7387388438834907";
+        public SmartPhone(string processor, int memory, int storage, string carrier, bool serviceActivated = false) : base(processor, memory, storage)
+        {
+            Carrier = carrier;
+            this.serviceActivated = serviceActivated;
+        }
+
+        public void ActivateService()
+        {
+            serviceActivated = true;
+        }
+
+        public void DeactivateService()
+        {
+            serviceActivated = false;
+        }
+
+        public bool GetServiceStatus()
+        {
+            return serviceActivated;
+        }
     }
 }
